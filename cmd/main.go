59dc22f4e8157f59d7c8f02f10d0a3c4877009cc
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-12-30 00:11:07 +0000

    This program is mainly for testing

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/DavidLoftus/highsociety"
+import (
+	"flag"
+	"fmt"
+	"github.com/DavidLoftus/highsociety"
+	"log"
+)
+
+var flNumUsers = flag.Int("users", 0, "number of users playing")
 
 func main() {
-	highsociety.NewGame()
+	gameState := highsociety.NewGame(*flNumUsers)
+
+	for !gameState.GameOver() {
+		player := gameState.CurrentPlayer()
+
+		fmt.Printf("Player %d, it's your turn.", player+1)
+
+		_, err := fmt.Scanln()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 }
